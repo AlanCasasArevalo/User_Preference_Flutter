af4be537fbc23b1d3faeb8721e32708a64577516
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-17 20:27:56 +0100

    [FEATURE][MENU] * New Menu was added

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:user_preference_app/src/page/settings_page.dart';
 
 class HomePage extends StatelessWidget {
-
   static final String routeName = 'home_page';
 
   @override
@@ -10,6 +10,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferencias de usuario'),
       ),
+      drawer: _builderMenu(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -19,6 +20,54 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text('Nombre del usuario: '),
           Divider(),
+        ],
+      ),
+    );
+  }
+
+  Drawer _builderMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/menu_img.jpg'),
+                    fit: BoxFit.cover)
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.home,
+              color: Colors.blue,
+            ),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, HomePage.routeName);
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.people,
+              color: Colors.blue,
+            ),
+            title: Text('Party mode'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: Colors.blue,
+            ),
+            title: Text('Ajustes'),
+            onTap: () {
+              // Navigator.pop(context);
+              // Navigator.pushNamed(context, SettingsPage.routeName);
+              Navigator.pushReplacementNamed(context, SettingsPage.routeName);
+            },
+          ),
         ],
       ),
     );
